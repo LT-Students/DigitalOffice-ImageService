518ccd3a49d0c0efc9f25b018b1fd69c0fed7258
VLiamin <LyaminVA@yandex.ru>
2021-08-30 19:08:24 +0300

    delete some fields from rabbitMqConfiq

--- src/ImageService.Models.Dto/Configuration/RabbitMqConfig.cs
+++ src/ImageService.Models.Dto/Configuration/RabbitMqConfig.cs
@@ -4,17 +4,6 @@ namespace LT.DigitalOffice.ImageService.Models.Dto.Configuration
 {
     public class RabbitMqConfig : BaseRabbitMqConfig
     {
-        public string CreateImagesProjectEndpoint { get; set; }
-        public string GetImagesProjectEndpoint { get; set; }
-        public string DeleteImagesProjectEndpoint { get; set; }
-        public string CreateImagesMessageEndpoint { get; set; }
-        public string GetImagesMessageEndpoint { get; set; }
-        public string DeleteImagesMessageEndpoint { get; set; }
-        public string CreateImagesNewsEndpoint { get; set; }
-        public string GetImagesNewsEndpoint { get; set; }
-        public string DeleteImagesNewsEndpoint { get; set; }
-        public string CreateImagesUserEndpoint { get; set; }
-        public string GetImagesUserEndpoint { get; set; }
-        public string DeleteImagesUserEndpoint { get; set; }
+        public string DeleteImagesEndpoint { get; set; }
     }
 }
