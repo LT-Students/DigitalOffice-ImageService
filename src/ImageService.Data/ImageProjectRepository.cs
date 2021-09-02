424ee95bbbfa52c7a6d80f58d424a0b0df0dcac7
VLiamin <43515136+VLiamin@users.noreply.github.com>
2021-09-02 15:44:51 +0300

    Change remove method in all repositories (#14)
    * change remove method in all repositories
    
    * delete library
    
    * change initialisation of table name
    
    * write UpCase
    
    * delete variable
    
    * fixed create method and delete SqlCommand
    
    * fixed variable
    
    * delete variable
    
    * fixed formalization
    
    * add check about null element

--- src/ImageService.Data/ImageProjectRepository.cs
+++ src/ImageService.Data/ImageProjectRepository.cs
@@ -1,78 +1,58 @@
-﻿using LT.DigitalOffice.ImageService.Data.Interfaces;
-using LT.DigitalOffice.ImageService.Data.Provider;
-using LT.DigitalOffice.ImageService.Models.Db;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using LT.DigitalOffice.ImageService.Data.Interfaces;
+using LT.DigitalOffice.ImageService.Data.Provider;
+using LT.DigitalOffice.ImageService.Models.Db;
 
 namespace LT.DigitalOffice.ImageService.Data
 {
-    public class ImageProjectRepository : IImageProjectRepository
+  public class ImageProjectRepository : IImageProjectRepository
+  {
+    private readonly IDataProvider _provider;
+
+    public ImageProjectRepository(IDataProvider provider)
     {
-        private readonly IDataProvider _provider;
-
-        public ImageProjectRepository(IDataProvider provider)
-        {
-            _provider = provider;
-        }
-
-        public List<Guid> Create(List<DbImageProject> imagesProjects)
-        {
-            if (imagesProjects.Contains(null))
-            {
-                return null;
-            }
-
-            _provider.ImagesProjects.AddRange(imagesProjects);
-            _provider.Save();
-
-            return imagesProjects.Select(x => x.Id).ToList();
-        }
-
-        public bool Remove(List<Guid> imageIds)
-        {
-            if (imageIds == null)
-            {
-                return false;
-            }
-
-            List<DbImageProject> imagesProjects = _provider.ImagesProjects
-                .Where(x => imageIds.Contains(x.Id) || (x.ParentId != null && imageIds.Contains((Guid)x.ParentId)))
-                .ToList();
-
-            if (imagesProjects == null)
-            {
-                return false;
-            }
-
-            List<Guid> parentIds = new();
-
-            foreach (DbImageProject imageProject in imagesProjects)
-            {
-                if (imageProject.ParentId != null
-                    && imageProject.Id != imageProject.ParentId
-                    && !imageIds.Contains((Guid)imageProject.ParentId))
-                {
-                    parentIds.Add((Guid)imageProject.ParentId);
-                }
-            }
-
-            imagesProjects.AddRange(_provider.ImagesProjects.Where(x => parentIds.Contains(x.Id)));
-
-            _provider.ImagesProjects.RemoveRange(imagesProjects);
-            _provider.Save();
-
-            return true;
-        }
-
-        public List<DbImageProject> Get(List<Guid> imageIds)
-        {
-            return _provider.ImagesProjects.Where(x => imageIds.Contains(x.Id)).ToList();
-        }
-
-        public DbImageProject Get(Guid imageId)
-        {
-            return _provider.ImagesProjects.FirstOrDefault(x => x.Id == imageId);
-        }
+      _provider = provider;
     }
+
+    public List<Guid> Create(List<DbImageProject> imagesProjects)
+    {
+      if (imagesProjects == null || !imagesProjects.Any() || imagesProjects.Contains(null))
+      {
+        return null;
+      }
+
+      _provider.ImagesProjects.AddRange(imagesProjects);
+      _provider.Save();
+
+      return imagesProjects.Select(x => x.Id).ToList();
+    }
+
+    public bool Remove(List<Guid> imageIds)
+    {
+      if (imageIds == null)
+      {
+        return false;
+      }
+
+      foreach (Guid imageId in imageIds)
+      {
+        _provider.ExecuteRawSql($@"DELETE FROM {DbImageProject.TableName} WHERE Id = '{imageId}' OR ParentId = '{imageId}' OR
+            Id IN (SELECT ParentId FROM ImagesProjects WHERE Id = '{imageId}' AND ParentId IS NOT NULL);");
+      }
+
+      return true;
+    }
+
+    public List<DbImageProject> Get(List<Guid> imageIds)
+    {
+      return _provider.ImagesProjects.Where(x => imageIds.Contains(x.Id)).ToList();
+    }
+
+    public DbImageProject Get(Guid imageId)
+    {
+      return _provider.ImagesProjects.FirstOrDefault(x => x.Id == imageId);
+    }
+  }
 }
--- src/ImageService.Models.Db/DbImageNews.cs
+++ src/ImageService.Models.Db/DbImageNews.cs
@@ -1,39 +1,39 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
 
 namespace LT.DigitalOffice.ImageService.Models.Db
 {
-    public class DbImageNews
+  public class DbImageNews
+  {
+    public const string TableName = "ImagesNews";
+
+    public Guid Id { get; set; }
+    public Guid? ParentId { get; set; }
+    public string Name { get; set; }
+    public string Content { get; set; }
+    public string Extension { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public Guid CreatedBy { get; set; }
+  }
+
+  public class DbImageNewsConfiguration : IEntityTypeConfiguration<DbImageNews>
+  {
+    public void Configure(EntityTypeBuilder<DbImageNews> builder)
     {
-        public const string TableName = "ImagesNews";
+      builder
+          .ToTable(DbImageNews.TableName);
 
-        public Guid Id { get; set; }
-        public Guid? ParentId { get; set; }
-        public string Name { get; set; }
-        public string Content { get; set; }
-        public string Extension { get; set; }
-        public DateTime CreatedAtUtc { get; set; }
-        public Guid CreatedBy { get; set; }
+      builder
+          .HasKey(a => a.Id);
+
+      builder
+          .Property(a => a.Content)
+          .IsRequired();
+
+      builder
+          .Property(a => a.Extension)
+          .IsRequired();
     }
-
-    public class DbImageNewsConfiguration : IEntityTypeConfiguration<DbImageNews>
-    {
-        public void Configure(EntityTypeBuilder<DbImageNews> builder)
-        {
-            builder
-                .ToTable(DbImageNews.TableName);
-
-            builder
-                .HasKey(a => a.Id);
-
-            builder
-                .Property(a => a.Content)
-                .IsRequired();
-
-            builder
-                .Property(a => a.Extension)
-                .IsRequired();
-        }
-    }
+  }
 }
