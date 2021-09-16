75921772f279046139b90cec20163d41fd1f2d1f
ndoriomedov <doriomedov213@gmail.com>
2021-09-17 01:35:22 +0300

    fix dbImage mappers

--- src/ImageService.Mappers/Db/DbImageProjectMapper.cs
+++ src/ImageService.Mappers/Db/DbImageProjectMapper.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 using LT.DigitalOffice.ImageService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.ImageService.Models.Db;
-using LT.DigitalOffice.Kernel.Extensions;
 using LT.DigitalOffice.Models.Broker.Models;
-using Microsoft.AspNetCore.Http;
 
 namespace LT.DigitalOffice.ImageService.Mappers.Db
 {
   public class DbImageProjectMapper : IDbImageProjectMapper
   {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public DbImageProjectMapper(IHttpContextAccessor httpContextAccessor)
-    {
-      _httpContextAccessor = httpContextAccessor;
-    }
-
     public DbImageProject Map(CreateImageData createImageData, Guid? parentId = null, string content = null)
     {
       if (createImageData == null)
@@ -31,7 +22,7 @@ namespace LT.DigitalOffice.ImageService.Mappers.Db
         Content = content ?? createImageData.Content,
         Extension = createImageData.Extension,
         CreatedAtUtc = DateTime.UtcNow,
-        CreatedBy = _httpContextAccessor.HttpContext.GetUserId()
+        CreatedBy = createImageData.CreatedBy
       };
     }
   }
--- src/ImageService.Mappers/Db/DbImageUserMapper.cs
+++ src/ImageService.Mappers/Db/DbImageUserMapper.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 using LT.DigitalOffice.ImageService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.ImageService.Models.Db;
-using LT.DigitalOffice.Kernel.Extensions;
 using LT.DigitalOffice.Models.Broker.Models;
-using Microsoft.AspNetCore.Http;
 
 namespace LT.DigitalOffice.ImageService.Mappers.Db
 {
   public class DbImageUserMapper : IDbImageUserMapper
   {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public DbImageUserMapper(IHttpContextAccessor httpContextAccessor)
-    {
-      _httpContextAccessor = httpContextAccessor;
-    }
-
     public DbImageUser Map(CreateImageData createImageData, Guid? parentId = null, string content = null)
     {
       if (createImageData == null)
@@ -31,7 +22,7 @@ namespace LT.DigitalOffice.ImageService.Mappers.Db
         Content = content ?? createImageData.Content,
         Extension = createImageData.Extension,
         CreatedAtUtc = DateTime.UtcNow,
-        CreatedBy = _httpContextAccessor.HttpContext.GetUserId()
+        CreatedBy = createImageData.CreatedBy
       };
     }
   }
