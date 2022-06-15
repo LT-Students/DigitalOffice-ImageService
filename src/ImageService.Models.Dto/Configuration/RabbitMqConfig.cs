19b620122ee9418a75ad8d999faba6da5ba207de
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-06-16 01:56:24 +0300

    fixes

--- src/ImageService.Models.Dto/Configuration/RabbitMqConfig.cs
+++ src/ImageService.Models.Dto/Configuration/RabbitMqConfig.cs
@@ -4,7 +4,6 @@ namespace LT.DigitalOffice.ImageService.Models.Dto.Configuration
 {
   public class RabbitMqConfig : BaseRabbitMqConfig
   {
-    public string RemoveImagesEndpoint { get; set; }
     public string CreateImagesEndpoint { get; set; }
     public string GetImagesEndpoint { get; set; }
   }
