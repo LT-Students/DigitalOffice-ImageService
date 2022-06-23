a9c85a5d939cbc1cfb847f47f7a24766f682ecae
AnnPostnikova <103537267+AnnPostnikova@users.noreply.github.com>
2022-06-23 19:38:50 +0300

    add get image endpoint (#31)
    * add get image endpoint
    
    * update version in startup
    
    * update spec version

---
+++ src/ImageService.Business/Commands/Image/Interfaces/IGetImageCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using LT.DigitalOffice.ImageService.Models.Dto.Responses;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.Models.Broker.Enums;
+
+namespace LT.DigitalOffice.ImageService.Business.Commands.Image.Interfaces
+{
+  [AutoInject]
+  public interface IGetImageCommand
+  {
+    Task<OperationResultResponse<ImageResponse>> ExecuteAsync(Guid imageId, ImageSource source);
+  }
+}
